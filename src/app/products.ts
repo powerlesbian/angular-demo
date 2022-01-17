49209e1f7c467c47998df5ab5a54adaac2515870
G Chao <81912588+powerlesbian@users.noreply.github.com>
2022-01-17 14:32:09 +0800

    first commit

---
+++ src/app/products.ts
@@ -0,0 +1,35 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'G extra huge boogie',
+    price: 799,
+    description:
+      'A large white yellowish human particulate with one of the best flavours!',
+  },
+  {
+    id: 2,
+    name: 'Boogie Mini',
+    price: 699,
+    description:
+      'A wonderful human particulate with one of the best potentials for collection.',
+  },
+  {
+    id: 3,
+    name: 'Boogie Standard',
+    price: 299,
+    description: 'A staple for every alien wanting to own a piece of human.',
+  },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
